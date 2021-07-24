16b6cb605239ee04093a94cf5fbdb32e2b48bba5
risyad19 <risyad.dimas@gmail.com>
2021-07-24 19:48:26 +0700

    Update tampilan laporan

--- application/controllers/Admin/Laporan/LaporanProfit.php
+++ application/controllers/Admin/Laporan/LaporanProfit.php
@@ -23,7 +23,7 @@ class LaporanProfit extends CI_Controller
     {
         $data['title'] = "Laporan Profit";
 
-        $this->load->view('Template/Head', $data);
+        $this->load->view('Template/HeadDataTablesJS', $data);
         $this->load->view('PageAdmin/Laporan/LaporanProfit', $data);
     }
 }
